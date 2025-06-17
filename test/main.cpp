57d386fb06e4a4351f7ee707c4ce7026fdb32065
apeishuai <whswhswhs66@gmail.com>
2025-06-17 16:32:49 +0800

    concurrent work fine

--- test/main.cpp
+++ test/main.cpp
@@ -1,23 +1,54 @@
 #include <QApplication>
-#include <QThread>
-#include <QtConcurrent/QtConcurrent>
+#include <QtConcurrent>
+#include <QMainWindow>
+#include <QDebug>
+#include <QLabel>
+#include <QTimer>
+#include <QRandomGenerator>
 
-void printMessage(const QString& message) {
-    qDebug() << "Message from worker thread ("
-             << QThread::currentThreadId() << "):" << message;
+// 数据处理函数 - 生成随机数
+QString generateData() {
+    QThread::msleep(40); // 模拟耗时操作（0.5秒）
+    int randomNum = QRandomGenerator::global()->bounded(1, 100); // 生成1-99的随机数
+    return QString("Random number: %1\nThread ID: %2")
+           .arg(randomNum)
+           .arg(reinterpret_cast<quintptr>(QThread::currentThreadId()));
 }
 
-    
+class MainWindow : public QMainWindow {
+public:
+    MainWindow(QWidget *parent = nullptr) : QMainWindow(parent) {
+        label = new QLabel("Initializing...", this);
+        label->setAlignment(Qt::AlignCenter);
+        setCentralWidget(label);
+        resize(300, 150);
+        
+        // 设置定时器定期生成数据
+        QTimer *timer = new QTimer(this);
+        connect(timer, &QTimer::timeout, this, &MainWindow::startDataGeneration);
+        timer->start(25); 
+    }
+
+    void startDataGeneration() {
+        QFuture<QString> future = QtConcurrent::run(generateData);
+        QFutureWatcher<QString>* watcher = new QFutureWatcher<QString>(this);
+        connect(watcher, &QFutureWatcher<QString>::finished, [this, watcher]() {
+            label->setText(watcher->result());
+            watcher->deleteLater();
+        });
+        watcher->setFuture(future);
+    }
+
+private:
+    QLabel *label;
+};
+
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     
-    qDebug() << "Main thread (" << QThread::currentThreadId() << ") started";
-
-    QFuture<void> future = QtConcurrent::run(printMessage,QString("Hello from QtConcurrent!"));
-    future.waitForFinished();
-
-    qDebug() << "Main thread finished";
+    MainWindow w;
+    w.setWindowTitle("Random Number Generator");
+    w.show();
 
     return app.exec();
 }
-
